8780eb9215bf12e6aa9925321d693cb073c44918
Tom ANDRE <tom.andre.dev@outlook.com>
2023-06-29 19:17:30 +0200

    #7 Bug fix

--- src/components/BoardController/button.jsx
+++ src/components/BoardController/button.jsx
@@ -1,7 +1,7 @@
 export default function BoardControlButton({ id, click, buttonContent, text }) {
   return (
-    <div className="BoardControl--params-layout">
-      <button key={id} onClick={click} className="BoardControl__button">
+    <div onClick={click} className="BoardControl--params-layout">
+      <button key={id} className="BoardControl__button">
         {buttonContent}
       </button>
       {text !== undefined ? <p>{text}</p> : undefined}
